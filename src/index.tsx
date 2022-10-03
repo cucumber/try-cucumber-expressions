b290e7edf42edfa0cd4f41994e17bccd73fcbb21
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2022-10-03 14:11:48 +0100

    Update dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { QueryParamProvider } from 'use-query-params'
+import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6'
 
 import { Try } from './Try.js'
 
@@ -11,13 +12,20 @@ if (!container) {
 }
 const root = createRoot(container)
 root.render(
-  <Router>
-    <QueryParamProvider ReactRouterRoute={Route}>
-      <Try
-        defaultExpressionText={'there is/are {int} flight(s) from {airport}'}
-        defaultStepText={'there are 12 flights from LHR'}
-        defaultParameters={[{ name: 'airport', regexp: '[A-Z]{3}' }]}
-      />
+  <BrowserRouter>
+    <QueryParamProvider adapter={ReactRouter6Adapter}>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Try
+              defaultExpressionText={'there is/are {int} flight(s) from {airport}'}
+              defaultStepText={'there are 12 flights from LHR'}
+              defaultParameters={[{ name: 'airport', regexp: '[A-Z]{3}' }]}
+            />
+          }
+        />
+      </Routes>
     </QueryParamProvider>
-  </Router>
+  </BrowserRouter>
 )
